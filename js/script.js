896c824a5fd85c4a2547a94ab5bb645d65fa0fb5
Rostyslav <roskillrk@gmail.com>
2024-08-05 15:30:05 +0300

    update2

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img class="leftimg" src="img/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img class="rightimg"src="img/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
